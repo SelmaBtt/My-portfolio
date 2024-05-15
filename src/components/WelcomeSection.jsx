f0cf7b0918760a11978f419e095f0bf597c406e2
Selma Brattli <selma@brattli.nu>
2024-05-15 14:17:33 +0200

    Basic welcome section started

--- src/components/WelcomeSection.jsx
+++ src/components/WelcomeSection.jsx
@@ -1,13 +1,25 @@
 import styled from '../stylesheets/WelcomeSection.module.css'
+import me from '../assets/my-face.png';
 
 const WelcomeSection = () => {
     return(
         <div className={styled.divWrapper}>
             <header className={styled.header}>
-                <h1 className={styled.title}>
+                <h2 className={styled.title}>
                     Welcome
-                </h1>
+                </h2>
             </header>
+            <div className={styled.div}>
+                <h1>
+                    Selma Brattli
+                </h1>
+                <p>
+                    Front-end developer
+                </p>
+                <div>
+                    <img src={me} alt="my face image" />
+                </div>
+            </div>
         </div>
     )
 }
