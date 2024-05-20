d64e02b8c6f057b897863a493ef02c6b3a4488d5
Selma Brattli <selma@brattli.nu>
2024-05-20 09:19:50 +0200

    fixed nav css problem

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,17 @@ import { useState } from 'react'
 import '../stylesheets/Root.css'
 import Nav from './NavBar/Nav'
 import WelcomeSection from './WelcomeSection'
+import styled from '../stylesheets/App.module.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div className={styled.appContainer}>
       <Nav />
       <WelcomeSection />
-    </>
+    </div>
   )
 }
 
