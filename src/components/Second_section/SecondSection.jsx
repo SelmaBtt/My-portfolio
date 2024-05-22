1218ed2565b3efaaacc41e91f1f76a15aca55fad
Selma Brattli <selma@brattli.nu>
2024-05-22 16:00:36 +0200

    positioning experience section

--- src/components/Second_section/SecondSection.jsx
+++ src/components/Second_section/SecondSection.jsx
@@ -1,14 +1,21 @@
 import AboutMe from "./AboutMe";
 import Languages from "./Languages";
 import Skills from "./Skills";
+import styled from '../../stylesheets/Second_section_stylesheets/SecondSection.module.css'
 
 const SecondSection = () => {
     return(
-        <>
-            <AboutMe />
-            <Languages />
-            <Skills />
-        </>
+        <div className={styled.container}>
+            <div className={styled.firstElement}>
+                <AboutMe />
+            </div>
+            <div className={styled.secondElement}>
+                <Languages />
+            </div>
+            <div className={styled.thirdElement}>
+                <Skills />
+            </div>
+        </div>
     )
 }
 
