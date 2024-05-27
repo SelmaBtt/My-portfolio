64e836e481709712a11b565164340feed0b41e17
Selma Brattli <selma@brattli.nu>
2024-05-27 17:30:41 +0200

    projects imported

---
+++ src/assets/ProjectList.js
@@ -0,0 +1,26 @@
+const allprojects = [
+    {
+        title: "Delicious meals",
+        skills: [
+            "React",
+            "JavaScript",
+            "HTML & CSS",
+            "Online API",
+        ],
+        description: "A simple project to fetch a online API and display meals and their details. The project that inspired this portfolio theme.",
+        img: '/list_images/DelMeal.png'
+    },
+    {
+        title: "The T-shirt Hub",
+        skills: [
+            "React",
+            "JavaScript",
+            "HTML & CSS",
+            "MySQL",
+        ],
+        description: "A fullstack project creating an online T-shirt store. It was my first attempt at backend. You can create an account, add and order a product.",
+        img: '/list_images/THub.png'
+    }
+];
+
+export default allprojects;
