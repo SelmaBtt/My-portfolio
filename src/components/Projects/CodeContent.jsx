15c995926db371c7d457a6524a474cebb8a9cc3f
Selma Brattli <selma@brattli.nu>
2024-05-29 09:45:24 +0200

    Done with the bulk of the slide show

--- src/components/Projects/CodeContent.jsx
+++ src/components/Projects/CodeContent.jsx
@@ -22,23 +22,23 @@ const CodeContent = () => {
 
     return (
         <div className={styled.contentWrapper}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" viewBox="0 0 45 45" fill="none">
+            <svg className={styled.info} xmlns="http://www.w3.org/2000/svg" width="45" height="45" viewBox="0 0 45 45" fill="none">
                 <path d="M22.5 15V10M22.5 35V20M10 2.5H35M37.5 5V10M42.5 10V35M37.5 35V40M35 42.5H10M7.5 40V35M2.5 35V10M7.5 10V5" stroke="#6B6B6B" strokeWidth="5"/>
             </svg>
-            <svg xmlns="http://www.w3.org/2000/svg" width="215" height="46" viewBox="0 0 215 46" fill="none">
+            <svg className={styled.demoBtn} xmlns="http://www.w3.org/2000/svg" width="215" height="46" viewBox="0 0 215 46" fill="none">
                 <path d="M10.5 3L205 2.5M207.5 5V10M212.5 10V35M207.5 35V40M205 42.5L10.5 43M8 40.5V35.5M3 35.5V10.5M8 10.5V5.5" stroke="#6B6B6B" strokeWidth="5"/>
                 <text x="50" y="32" fontSize="20" fill="black">Live demo</text>
             </svg>
-            <h2>{currentProject.title}</h2>
-            <p>{currentProject.description}</p>
+            <h2 className={styled.proTitle}>{currentProject.title}</h2>
+            <p className={styled.description}>{currentProject.description}</p>
             <img className={styled.img} src={currentProject.img} alt="project image" />
 
             {/* Buttons */}
-            <svg onClick={previousSlideHandler} xmlns="http://www.w3.org/2000/svg" width="106" height="66" viewBox="0 0 106 66" fill="none">
-                <path d="M15.5 3H90.5M90.5 8H95.5M98 10.5V20.5M103 20.5V45.5M98 45.5V55.5M93 55.5V60.5M90.5 63H15.5M15.5 58H10.5M8 55.5V45.5M3 45.5V20.5M8 20.5V10.5M13 10.5V5.5M50.5 18H45.5M45.5 23H40.5M40.5 28H35.5M35.5 33H30.5M40.5 38H35.5M45.5 43H40.5M50.5 48H45.5M70.5 48H65.5M65.5 43H60.5M60.5 38H55.5M55.5 33H50.5M60.5 28H55.5M65.5 23H60.5M70.5 18H65.5" stroke="#6B6B6B" stroke-width="5"/>
+            <svg className={styled.preBtn} onClick={previousSlideHandler} xmlns="http://www.w3.org/2000/svg" width="106" height="66" viewBox="0 0 106 66" fill="none">
+                <path d="M15.5 3H90.5M90.5 8H95.5M98 10.5V20.5M103 20.5V45.5M98 45.5V55.5M93 55.5V60.5M90.5 63H15.5M15.5 58H10.5M8 55.5V45.5M3 45.5V20.5M8 20.5V10.5M13 10.5V5.5M50.5 18H45.5M45.5 23H40.5M40.5 28H35.5M35.5 33H30.5M40.5 38H35.5M45.5 43H40.5M50.5 48H45.5M70.5 48H65.5M65.5 43H60.5M60.5 38H55.5M55.5 33H50.5M60.5 28H55.5M65.5 23H60.5M70.5 18H65.5" stroke="#6B6B6B" strokeWidth="5"/>
             </svg>
-            <svg onClick={nextSlideHandler} xmlns="http://www.w3.org/2000/svg" width="106" height="66" viewBox="0 0 106 66" fill="none">
-                <path d="M15.5 3H90.5M90.5 8H95.5M98 10.5V20.5M103 20.5V45.5M98 45.5V55.5M93 55.5V60.5M90.5 63H15.5M15.5 58H10.5M8 55.5V45.5M3 45.5V20.5M8 20.5V10.5M13 10.5V5.5M36 17.5H41M41 22.5H46M46 27.5H51M51 32.5H56M46 37.5H51M41 42.5H46M36 47.5H41M56 47.5H61M61 42.5H66M66 37.5H71M71 32.5H76M66 27.5H71M61 22.5H66M56 17.5H61" stroke="#6B6B6B" stroke-width="5"/>
+            <svg className={styled.nexBtn} onClick={nextSlideHandler} xmlns="http://www.w3.org/2000/svg" width="106" height="66" viewBox="0 0 106 66" fill="none">
+                <path d="M15.5 3H90.5M90.5 8H95.5M98 10.5V20.5M103 20.5V45.5M98 45.5V55.5M93 55.5V60.5M90.5 63H15.5M15.5 58H10.5M8 55.5V45.5M3 45.5V20.5M8 20.5V10.5M13 10.5V5.5M36 17.5H41M41 22.5H46M46 27.5H51M51 32.5H56M46 37.5H51M41 42.5H46M36 47.5H41M56 47.5H61M61 42.5H66M66 37.5H71M71 32.5H76M66 27.5H71M61 22.5H66M56 17.5H61" stroke="#6B6B6B" strokeWidth="5"/>
             </svg>
         </div>
     );
