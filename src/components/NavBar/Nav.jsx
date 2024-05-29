9f8fe9d5de9bdca922e798f113ff7fb8ce9485ff
Selma Brattli <selma@brattli.nu>
2024-05-30 00:22:03 +0200

    Nav done

--- src/components/NavBar/Nav.jsx
+++ src/components/NavBar/Nav.jsx
@@ -1,13 +1,55 @@
 import SvgIcon from "./SvgIcon";
 import styled from '../../stylesheets/Nav.module.css'
+import { useState } from "react";
 
 const Nav = () => {
+    const [isStartTrue, setIsStartTrue] = useState(false)
+    const [isAboutMeTrue, setIsAboutMeTrue] = useState(false)
+    const [isProjectsTrue, setIsProjectsTrue] = useState(false)
+    const [isContactTrue, setIsContactTrue] = useState(false)
+
     return(
         <div className={styled.NavWrapper}>
-            <SvgIcon />
-            <SvgIcon />
-            <SvgIcon />
-            <SvgIcon />
+            <div
+            onMouseEnter={() => setIsStartTrue(true)}
+            onMouseLeave={() => setIsStartTrue(false)}>
+                <SvgIcon />
+                {isStartTrue && 
+                    <div className={styled.startModal} onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}>
+                        <h2>Start</h2>
+                    </div>
+                }
+            </div>
+            <div
+            onMouseEnter={() => setIsAboutMeTrue(true)}
+            onMouseLeave={() => setIsAboutMeTrue(false)}>
+                <SvgIcon />
+                {isAboutMeTrue && 
+                    <div className={styled.aboutMeModal} onClick={() => window.scrollTo({ top: 1600, behavior: "smooth" })}>
+                        <h2>About me</h2>
+                    </div>
+                }
+            </div>
+            <div
+            onMouseEnter={() => setIsProjectsTrue(true)}
+            onMouseLeave={() => setIsProjectsTrue(false)}>
+                <SvgIcon />
+                {isProjectsTrue && 
+                    <div className={styled.projectsModal} onClick={() => window.scrollTo({ top: 2900, behavior: "smooth" })}>
+                        <h2>Projects</h2>
+                    </div>
+                }
+            </div>
+            <div
+            onMouseEnter={() => setIsContactTrue(true)}
+            onMouseLeave={() => setIsContactTrue(false)}>
+                <SvgIcon />
+                {isContactTrue && 
+                    <div className={styled.contactModal} onClick={() => window.scrollTo({ top: document.body.scrollHeight, behavior: "smooth" })}>
+                        <h2>Contact</h2>
+                    </div>
+                }
+            </div>
         </div>
     )
 }
