1590cf4b6d1dac879c6f585a008a627448f3df6a
Selma Brattli <selma@brattli.nu>
2024-09-09 19:25:41 +0200

    Skills list update

--- src/components/Second_section/Skills.jsx
+++ src/components/Second_section/Skills.jsx
@@ -7,17 +7,16 @@ const Skills = () => {
                 <h1>Skills</h1>
             </header>
             <div className={styled.textContainer}>
-                <ul>
-                    <li>HTML & CSS</li>
-                    <li>JavaScript</li>
-                    <li>React.js</li>
-                    <li>Figma</li>
-                    <li>Github</li>
-                    <li>API</li>
-                    <li>Node</li>
-                    <li>Cypress</li>
-                    <li>SQL</li>
-                </ul>
+            <ul>
+                <li>HTML & CSS</li>
+                <li>JavaScript <span>(React.js, Node, Express)</span></li>
+                <li>Version Control <span>(Git, Github)</span></li>
+                <li>Design & Prototyping <span>(Figma)</span></li>
+                <li>Testing <span>(Cypress)</span></li>
+                <li>Databases <span>(MySQL, SQL)</span></li>
+                <li>Build Tools <span>(Vite)</span></li>
+                <li>API Integration</li>
+            </ul>
             </div>
         </div>
     )
